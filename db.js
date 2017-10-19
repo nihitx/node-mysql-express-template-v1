fd78c96b57e7fcc8af59269c2dd33030f08ac0f4
nihitx <masnadhossain@live.com>
2017-10-19 22:44:25 +0300

    db changes since mysql loses connectin on heroku

--- db.js
+++ db.js
@@ -1,13 +1,35 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var db_config = {
     host     : '127.0.0.1', // Your host - either local or cloud
     user     : 'root', // your username
     password : 'root', // your password
     database : 'your-db-name' // database name
-});
-connection.connect(function(err) {
-    if (err) throw err;
-});
+};
+
+var connection;
+
+function handleDisconnect() {
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                    // the old one cannot be reused.
+
+    connection.connect(function(err) {              // The server is either down
+        if(err) {                                     // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+                                            // If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+
+handleDisconnect();
 
 module.exports = connection;
